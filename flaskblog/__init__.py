ba5701c1560ee59bb33e8d03fdcce30b8b9efa05
Fung Yeung <yeung.kei.fung@gmail.com>
2023-01-04 17:45:26 +0800

    completed part 6

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "0360ec4009124e0e177d8eeee10a49be"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app=app)
+bcrypt = Bcrypt(app=app)
+login_manager = LoginManager(app=app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from flaskblog import routes
